bd11c27cff403fb1cac54b9667dd7b3d93bb8f90
Alfajri Asnan Kusuma <alfajriaskus@gmail.com>
2020-12-20 15:43:30 +0800

    Add files via upload

---
+++ posisimouse.py
@@ -0,0 +1,13 @@
+#! python 3
+import pyautogui as gui
+
+
+try:
+    while True:
+        x, y = gui.position()
+        position = 'X: '+str(x).rjust(4)+'Y: '+str(y).rjust(4)
+        print(position, end='\n')
+except KeyboardInterrupt:
+    print('\nSelesai')
+
+
